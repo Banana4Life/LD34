ee0dfcef30d707037bfc69d4157d42a086d3d9b7
Anselm Brehme <development.anselm@breh.me>
2015-12-12 14:23:48 +0100

    SetupGame

---
+++ Assets/Scripts/SetupGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupGame : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform transform;
+	void Start () {
+
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Instantiate(transform, new Vector3(x * 2, y * 2, 0f), Quaternion.identity);
+            }
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
