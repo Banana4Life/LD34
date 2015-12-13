375162f1663d89eaff106953921c6a6bb36cb8f0
Jonas Dann <jonas.chr.dann@gmail.com>
2015-12-13 18:59:51 +0100

    Added rotation and flip to villages

--- Assets/Scripts/Village_Taken.cs
+++ Assets/Scripts/Village_Taken.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class Village_Taken : MonoBehaviour
 {
     public SpriteRenderer renderer;
+    public Sprite campAlien;
+    public Sprite campLegionnaire;
+    public Sprite villageAlien;
+    public Sprite villageLegionnaire;
     public Sprite castleAlien;
     public Sprite castleLegionnaire;
     private Village village;
@@ -12,25 +16,43 @@ public class Village_Taken : MonoBehaviour
     {
         village = gameObject.GetComponentInParent(typeof (Village)) as Village;
 
+        gameObject.transform.localEulerAngles = new Vector3(0, 0, 0);
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+
         if (village.faction != null)
         {
-            if (village.faction == Village.Faction.ENEMY)
+            if (village.size == Village.Size.CAMP)
             {
-                if (village.size == Village.Size.CASTLE)
+                if (village.faction == Village.Faction.ENEMY)
+                {
+                    renderer.sprite = campAlien;
+                }
+                else if (village.faction == Village.Faction.FRIENDLY)
+                {
+                    renderer.sprite = campLegionnaire;
+                }
+            }
+            else if (village.size == Village.Size.VILLAGE)
+            {
+                if (village.faction == Village.Faction.ENEMY)
+                {
+                    renderer.sprite = villageAlien;
+                }
+                else if (village.faction == Village.Faction.FRIENDLY)
+                {
+                    renderer.sprite = villageLegionnaire;
+                }
+            }
+            else if (village.size == Village.Size.CASTLE)
+            {
+                if (village.faction == Village.Faction.ENEMY)
                 {
                     renderer.sprite = castleAlien;
                 }
-            }
-            else if (village.faction == Village.Faction.FRIENDLY)
-            {
-                if (village.size == Village.Size.CASTLE)
+                else if (village.faction == Village.Faction.FRIENDLY)
                 {
                     renderer.sprite = castleLegionnaire;
                 }
-            }
-            else
-            {
-                renderer.sprite = null;
             }
         }
     }
