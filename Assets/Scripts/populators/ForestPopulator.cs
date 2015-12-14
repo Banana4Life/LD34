d2453c1816bf89888d6eb2121293b2ba2231d81d
Phillip Schichtel <phillip.public@schich.tel>
2015-12-14 12:22:49 +0100

    same refactoring and prepared a forest tile

---
+++ Assets/Scripts/populators/ForestPopulator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForestPopulator : GridPopulator
+{
+    public GameObject forestPrefab;
+
+    public override void populate(GameObject[,] gameObjects)
+    {
+        
+    }
+
+}
--- Assets/Scripts/tiles/TileObject.cs
+++ Assets/Scripts/tiles/TileObject.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public abstract class TileObject : MonoBehaviour {
+public abstract class TileObject : MonoBehaviour
+{
+    public double costToPass = 1d;
+    public bool passable = true;
+
     public virtual bool canBePassed()
     {
-        return true;
+        return passable;
     }
 
     public virtual double passingCost()
     {
-        return 1d;
+        return costToPass;
     }
 }
