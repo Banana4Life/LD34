b2f268f1bae68da414540dfc714d094c5a2fe074
Anselm Brehme <development.anselm@breh.me>
2015-12-14 03:17:46 +0100

    .

--- Assets/Scripts/tiles/Village.cs
+++ Assets/Scripts/tiles/Village.cs
@@ -19,8 +19,7 @@ public class Village : TileObject
     public bool flipY;
     public float angle;
 
-    public Vector3 defendingUnits;
-    public Vector3 attackingUnits;
+    public Vector3 defForce = new Vector3(10, 0, 0);
 
     private GameObject factionObject;
 
@@ -51,7 +50,8 @@ public class Village : TileObject
         this.faction = faction;
     }
 
-    void Start() {
+    void Start()
+    {
 
         if (this.size == null)
         {
@@ -68,6 +68,83 @@ public class Village : TileObject
 
         gameObject.transform.localEulerAngles = new Vector3(0, 0, angle);
         gameObject.transform.localScale = new Vector3(flipX ? -1 : 1, flipY ? -1 : 1, 1);
+    }
+
+    public int bonus = 4;
+
+    void fight(Vector3 atkForce, Faction faction)
+    {
+        if (this.faction == faction)
+        {
+            defForce += atkForce;
+        }
+        else
+        {
+            var defLossX = 0f;
+            var defLossY = 0f;
+            var defLossZ = 0f;
+            var atkLossX = 0f;
+            var atkLossY = 0f;
+            var atkLossZ = 0f;
+
+            float defMagnitude = defForce.x + defForce.y + defForce.z;
+            float atkMagnitude = atkForce.x + atkForce.y + atkForce.z;
+            // Attacker...
+            // Attack X (Melee)
+            defLossX += defForce.x / defMagnitude * atkForce.x * 1 * 1;
+            defLossY += defForce.x / defMagnitude * atkForce.y * 1 * 1;
+            defLossZ += defForce.x / defMagnitude * atkForce.z * 1 * bonus;
+            // Attack Y (Ranged)
+            defLossX += defForce.y / defMagnitude * atkForce.x * 1 * bonus;
+            defLossY += defForce.y / defMagnitude * atkForce.y * 1 * 1;
+            defLossZ += defForce.y / defMagnitude * atkForce.z * 1 * 1;
+            // Attack Z (Mounted)
+            defLossX += defForce.z / defMagnitude * atkForce.x * 1 * 1;
+            defLossY += defForce.z / defMagnitude * atkForce.y * 1 * bonus;
+            defLossZ += defForce.z / defMagnitude * atkForce.z * 1 * 1;
+            // Defender...
+            // Defend X (Melee)
+            atkLossX += atkForce.x / atkMagnitude * defForce.x * 1 * 1;
+            atkLossY += atkForce.x / atkMagnitude * defForce.y * 1 * 1;
+            atkLossZ += atkForce.x / atkMagnitude * defForce.z * 1 * bonus;
+            // Defend Y (Ranged)
+            atkLossX += atkForce.y / atkMagnitude * defForce.x * 1 * bonus;
+            atkLossY += atkForce.y / atkMagnitude * defForce.y * 1 * 1;
+            atkLossZ += atkForce.y / atkMagnitude * defForce.z * 1 * 1;
+            // Defend Z (Mounted)
+            atkLossX += atkForce.z / atkMagnitude * defForce.x * 1 * 1;
+            atkLossY += atkForce.z / atkMagnitude * defForce.y * 1 * bonus;
+            atkLossZ += atkForce.z / atkMagnitude * defForce.z * 1 * 1;
+
+            defForce -= new Vector3(defLossX, defLossY, defLossZ);
+            atkForce -= new Vector3(atkLossX, atkLossY, atkLossZ);
+
+            if (defForce.x < 0)
+            {
+                defForce.x = 0;
+            }
+            if (defForce.y < 0)
+            {
+                defForce.y = 0;
+            }
+            if (defForce.z < 0)
+            {
+                defForce.z = 0;
+            }
+
+            if (defForce.sqrMagnitude == 0)
+            {
+                Debug.Log("Attacker won: " + faction);
+                this.faction = faction;
+                defForce = atkForce;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(
+        collision.collider.name);
     }
 }
 
