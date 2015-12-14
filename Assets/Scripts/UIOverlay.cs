d7a0698705265f9a09d27f307f8dd0d2f4ab4dcb
Jonas Dann <jonas.chr.dann@gmail.com>
2015-12-14 23:51:20 +0100

    Added won and lost messages

---
+++ Assets/Scripts/UIOverlay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class UIOverlay : MonoBehaviour
+{
+    public static GameObject uiOverlay;
+    public static GameObject scrollUi;
+
+    void Start ()
+    {
+        uiOverlay = gameObject;
+        scrollUi = gameObject.transform.GetChild(0).gameObject;
+        for (int i = 0; i < uiOverlay.transform.childCount; i++)
+        {
+            uiOverlay.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    public void factionDefeated(Faction f)
+    {
+        if (f == Faction.ENEMY)
+        {
+            uiOverlay.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else if (f == Faction.FRIENDLY)
+        {
+            uiOverlay.transform.GetChild(2).gameObject.SetActive(true);
+        }
+    }
+}
