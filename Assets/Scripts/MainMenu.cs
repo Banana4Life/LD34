eaebf13d6f45f2a59c30b0ab2dc39b71d32021fd
Jonas Dann <jonas.chr.dann@gmail.com>
2015-12-15 03:19:46 +0100

    Added menu stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,21 +2,62 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-    public void GridReady()
+public class MainMenu : MonoBehaviour
+{
+    private bool difficulty;
+    private bool esc;
+
+    void Start()
     {
-        // do nothing
+        gameObject.transform.GetChild(6).gameObject.SetActive(false);
+        gameObject.transform.GetChild(7).gameObject.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0) && !difficulty)
+        {
+            gameObject.transform.GetChild(4).gameObject.SetActive(false);
+            gameObject.transform.GetChild(5).gameObject.SetActive(false);
+            gameObject.transform.GetChild(6).gameObject.SetActive(true);
+            gameObject.transform.GetChild(7).gameObject.SetActive(true);
+            difficulty = true;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            Application.Quit();
+        } 
+        else if (Input.GetKey(KeyCode.Alpha1) && difficulty)
         {
             SceneManager.LoadScene("Main");
         }
-        else if (Input.GetKey(KeyCode.Escape))
+        else if (Input.GetKey(KeyCode.Alpha2) && difficulty)
         {
-            Application.Quit();
+            SceneManager.LoadScene("Main");
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (!esc)
+            {
+                if (difficulty)
+                {
+                    gameObject.transform.GetChild(4).gameObject.SetActive(true);
+                    gameObject.transform.GetChild(5).gameObject.SetActive(true);
+                    gameObject.transform.GetChild(6).gameObject.SetActive(false);
+                    gameObject.transform.GetChild(7).gameObject.SetActive(false);
+                    difficulty = false;
+                }
+                else
+                {
+                    Application.Quit();
+                }
+            }
+            esc = true;
+        }
+        else
+        {
+            esc = false;
         }
     }
 }
